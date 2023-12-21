1be5da5b4acde0e367877701be78a7c9bcf947c3
Yasin Engin ALBAKIR <xyalbakir@havelsan.com.tr>
2023-12-21 08:45:15 +0300

    rest assured testng.xml added.

--- src/test/java/api/test/PostTests.java
+++ src/test/java/api/test/PostTests.java
@@ -1,4 +1,11 @@
 package api.test;
 
+import org.testng.annotations.Test;
+
 public class PostTests {
+
+    @Test
+    public void createUserPostTest() {
+
+    }
 }
--- src/test/java/api/utilities/DataProviders.java
+++ src/test/java/api/utilities/DataProviders.java
@@ -8,9 +8,10 @@ public class DataProviders {
 
     public static String path = System.getProperty("user.dir") + "//src//test//resources//testData//userData.xlsx";
     public static String userSheet = "Sheet1";
+    public static String postSheet = "Sheet2";
 
     @DataProvider(name = "user-data")
-    public String[][] getAllData() throws IOException {
+    public String[][] getAllUserData() throws IOException {
 
         ExcelUtility excelUtility = new ExcelUtility(path);
         int rownum = excelUtility.getRowCount(userSheet);
@@ -20,6 +21,22 @@ public class DataProviders {
         for (int i = 1; i <= rownum; i++) {
             for (int j = 0; j < colnum; j++) {
                 payload[i - 1][j] = excelUtility.getCellData(userSheet, i, j);
+            }
+        }
+        return payload;
+    }
+
+    @DataProvider(name = "post-data")
+    public String[][] getAllPostData() throws IOException {
+
+        ExcelUtility excelUtility = new ExcelUtility(path);
+        int rownum = excelUtility.getRowCount(postSheet);
+        int colnum = excelUtility.getCellCount(postSheet, 1);
+
+        String payload[][] = new String[rownum][colnum];
+        for (int i = 1; i <= rownum; i++) {
+            for (int j = 0; j < colnum; j++) {
+                payload[i - 1][j] = excelUtility.getCellData(postSheet, i, j);
             }
         }
         return payload;
